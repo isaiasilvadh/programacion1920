dc9fe1b6dac998c29f518b674e60d0063f1db02c
Isaiasilvadh <isaiasilva64@gmail.com>
2019-10-18 19:58:08 -0500

    version con operadores de incremento y decremento

--- JavaApplication1/src/javaapplication1/Operadores.java
+++ JavaApplication1/src/javaapplication1/Operadores.java
@@ -16,12 +16,32 @@ public class Operadores {
        double multiplicacion = 50 * 0.12;
        int division = 100 / 50;
        int residuo = 7 % 2;
-       
+        System.out.println("1. OPEADORES ARITMETICOS");
         System.out.println("La suma es " +suma);
         System.out.println("La resta es "+resta);
         System.out.println("la multiplicación es"+multiplicacion);
         System.out.println("la división es" +division);
         System.out.println("el residuo es"+residuo);
+        //2. OPERADORES DE INCREMENTO Y DECREMENTO
+        int nota = 15;
+        nota = nota + 1;
+        System.out.println("2. OPERADORES DE INCREMENTO Y DECREMENTO");
+        System.out.println("incrememento como nota = nota +1 :"+nota);
+        nota +=1;
+        System.out.println("Incremento como nota +=1 :" +nota);
+        nota++;
+        System.out.println("Incremento como nota++ :"+nota);
+        
+        nota = nota -1 ;
+        System.out.println("decremento de la variable nota");
+        System.out.println("decremento como nota = nota -1 :"+nota);
+        nota -= 1;
+        System.out.println("decremento como nota -=1:"+nota);
+        
+        nota--;
+        System.out.println("decremento como nota--:"+nota);
+        
+        
        
     }
     
