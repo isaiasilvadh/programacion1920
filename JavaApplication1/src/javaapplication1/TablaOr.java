50b8b70330020b9b3aa9235a0c278f227d0fc2a1
Isaiasilvadh <isaiasilva64@gmail.com>
2019-10-27 10:17:03 -0500

    Update TablaOr.java

--- JavaApplication1/src/javaapplication1/TablaOr.java
+++ JavaApplication1/src/javaapplication1/TablaOr.java
@@ -10,8 +10,9 @@ package javaapplication1;
  * @author isaia
  */
 public class TablaOr {
+
     public static void main(String[] args) {
-        
+
         System.out.println("TABLA DE VERDAD OR");
         System.out.println("|x \t y \t resultado");
         System.out.println("|____|____|___________________");
@@ -20,13 +21,16 @@ public class TablaOr {
         System.out.println("v \t f \t " + (true || false));
         System.out.println("|____|____|___________________");
         System.out.println("f \t v \t " + (false || true));
-       System.out.println("|____|____|___________________");
+        System.out.println("|____|____|___________________");
         System.out.println("f \t f \t " + (false || false));
-        
+
         System.out.println("TABLA DE VERDAD DEL NOT");
         System.out.println("x \t resultado");
-        System.out.println(true + "\t" +!true);
-        System.out.println(false + "\t" +!false);
+        System.out.println(true + "\t" + !true);
+        System.out.println(false + "\t" + !false);
+        
+   
+
     }
-    
+
 }
