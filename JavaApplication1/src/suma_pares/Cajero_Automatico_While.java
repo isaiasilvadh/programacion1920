a21152c1f17a6462c4d95a5f21edbd8be3b6da1e
Isaiasilvadh <isaiasilva64@gmail.com>
2019-11-22 18:33:00 -0500

    cajero

--- JavaApplication1/src/suma_pares/Cajero_Automatico_While.java
+++ JavaApplication1/src/suma_pares/Cajero_Automatico_While.java
@@ -12,77 +12,75 @@ import java.util.Scanner;
  * @author isaia
  */
 public class Cajero_Automatico_While {
-        public static void main(String[] args) {
-         Scanner teclado = new Scanner(System.in);
-       boolean nuevo_numero = true;
-       boolean ingreso = true; 
-        
-        while   (ingreso == true){
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        boolean nuevo_numero = true;
+        boolean ingreso = true;
+
+        while (ingreso == true) {
             System.out.println("Desea seguir navegando si/no");
             String respuesta = teclado.next();
             respuesta = respuesta.toLowerCase();
-            
-
-        System.out.println("CAJERO AUTOMATICO -- BANCO DE LOJA");
-        System.out.println("Digita 1 para realizar un retiro ");
-        System.out.println("Digita 2 para realizar un deposito");
-        System.out.println("digita 3 para realizar una consulta de saldo");
-        System.out.println("Digita 0 si no quieres hacer nada");
-        
-        System.out.println("Ingresar valor incial de tu cuenta");
-        int saldo_cuenta = teclado.nextInt();
-        System.out.println("ingrese la opcion elejida");
-        int opcion = teclado.nextInt();
-
-        
             if (respuesta.equals("si")) {
-        
-        switch (opcion) {
-            case 1:
-                System.out.println("OPCIÓN PARA REALIZAR UN RETIRO");
-                System.out.println("ingrese valor a retirar");
-                int retiro = teclado.nextInt();
-                if (retiro <= saldo_cuenta) {
 
-                    saldo_cuenta = saldo_cuenta - retiro;
-                } else {
-                    System.out.println("FONDOS INSUFICIENTES");
-                }
+            System.out.println("CAJERO AUTOMATICO -- BANCO DE LOJA");
+            System.out.println("Digita 1 para realizar un retiro ");
+            System.out.println("Digita 2 para realizar un deposito");
+            System.out.println("digita 3 para realizar una consulta de saldo");
+            System.out.println("Digita 0 si no quieres hacer nada");
 
-                break;
-            case 2:
-                System.out.println("OPCIÓN PARA REALIZAR UN DEPOSITO");
-                System.out.println("ingrese el valor a depositar");
-                int deposito = teclado.nextInt();
-                if (deposito > 0) {
+            System.out.println("Ingresar valor incial de tu cuenta");
+            int saldo_cuenta = teclado.nextInt();
+            System.out.println("ingrese la opcion elejida");
+            int opcion = teclado.nextInt();
 
-                    saldo_cuenta = saldo_cuenta + deposito;
-                } else {
-                    System.out.println("esta intentando ingresar un valor negativo");
-                }
-                break;
-
-            case 3:
-                System.out.println("Saldo de cuenta");
-                System.out.println("su saldo es :$ " + saldo_cuenta);
-                break;
-            case 4:
-                break;
-        }
-        
-        }else if (respuesta.equals("no") ) {
-                ingreso = false;
-                
-            }
             
 
-        System.out.println("ahora tu saldo es de:" + saldo_cuenta);
-        
+                switch (opcion) {
+                    case 1:
+                        System.out.println("OPCIÓN PARA REALIZAR UN RETIRO");
+                        System.out.println("ingrese valor a retirar");
+                        int retiro = teclado.nextInt();
+                        if (retiro <= saldo_cuenta) {
+
+                            saldo_cuenta = saldo_cuenta - retiro;
+                        } else {
+                            System.out.println("FONDOS INSUFICIENTES");
+                        }
+
+                        break;
+                    case 2:
+                        System.out.println("OPCIÓN PARA REALIZAR UN DEPOSITO");
+                        System.out.println("ingrese el valor a depositar");
+                        int deposito = teclado.nextInt();
+                        if (deposito > 0) {
+
+                            saldo_cuenta = saldo_cuenta + deposito;
+                        } else {
+                            System.out.println("esta intentando ingresar un valor negativo");
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("Saldo de cuenta");
+                        System.out.println("su saldo es :$ " + saldo_cuenta);
+                        break;
+                    case 4:
+                        break;
+                }
+                System.out.println("ahora tu saldo es de:" + saldo_cuenta);
+
+            } else if (respuesta.equals("no")) {
+                ingreso = false;
+
+            }
+
+            
+
         }
-        
-            System.out.println("gracias por su visita tenga una buena tarde");
+
+        System.out.println("gracias por su visita tenga una buena tarde");
     }
 
 }
-    
-
